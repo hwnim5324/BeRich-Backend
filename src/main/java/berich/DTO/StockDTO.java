bc574a45f40ba9f03d3be1b763dd96496c26b252
남현우 <hwnim5324@naver.com>
2022-06-29 23:44:40 +0900

    make example codes.

---
+++ src/main/java/berich/DTO/StockDTO.java
@@ -0,0 +1,40 @@
+package berich.DTO;
+
+public class StockDTO {
+    private int STCK_HGPR;
+    private int STCK_LWPR;
+    private int STCK_OPRC;
+    private int STCK_CLPR;
+
+    public int getSTCK_HGPR() {
+        return STCK_HGPR;
+    }
+
+    public void setSTCK_HGPR(int STCK_HGPR) {
+        this.STCK_HGPR = STCK_HGPR;
+    }
+
+    public int getSTCK_LWPR() {
+        return STCK_LWPR;
+    }
+
+    public void setSTCK_LWPR(int STCK_LWPR) {
+        this.STCK_LWPR = STCK_LWPR;
+    }
+
+    public int getSTCK_OPRC() {
+        return STCK_OPRC;
+    }
+
+    public void setSTCK_OPRC(int STCK_OPRC) {
+        this.STCK_OPRC = STCK_OPRC;
+    }
+
+    public int getSTCK_CLPR() {
+        return STCK_CLPR;
+    }
+
+    public void setSTCK_CLPR(int STCK_CLPR) {
+        this.STCK_CLPR = STCK_CLPR;
+    }
+}
--- src/main/java/berich/controller/BeRichController.java
+++ src/main/java/berich/controller/BeRichController.java
@@ -1,8 +1,11 @@
 package berich.controller;
 
+import berich.DTO.StockDTO;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class BeRichController {
@@ -11,5 +14,16 @@ public class BeRichController {
     public String hello(Model model){
         model.addAttribute("data", "hello!!");
         return "hello";
+    }
+
+    @GetMapping("stocks")
+    @ResponseBody
+    public StockDTO StockApi(@RequestParam("name") String name){
+        StockDTO stock = new StockDTO();
+        stock.setSTCK_CLPR(123);
+        stock.setSTCK_HGPR(123);
+        stock.setSTCK_LWPR(123);
+        stock.setSTCK_OPRC(123);
+        return stock;
     }
 }
