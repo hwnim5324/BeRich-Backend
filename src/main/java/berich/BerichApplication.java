18b88ca3e3dbb539564e20233d699e7d4a8726fa
남현우 <hwnim5324@naver.com>
2022-06-29 22:38:49 +0900

    Settings for Backend of BeRich Project.

---
+++ src/main/java/berich/BerichApplication.java
@@ -0,0 +1,13 @@
+package berich;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BerichApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BerichApplication.class, args);
+	}
+
+}
