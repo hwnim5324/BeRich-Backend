cff0949087445cc20b142a3b77e2906fc5cb96eb
남현우 <hwnim5324@naver.com>
2022-07-02 23:41:51 +0900

    assets rest api in progress.

---
+++ src/main/java/berich/service/AssetsService.java
@@ -0,0 +1,16 @@
+package berich.service;
+
+import berich.DTO.AssetsDTO;
+
+public class AssetsService {
+    private AssetsDTO user;
+
+    public int updateAssets(AssetsDTO assets){
+        int stateCode = 0;
+
+
+
+        return stateCode;
+    }
+
+}
