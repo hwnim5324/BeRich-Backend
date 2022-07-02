c06c5a4cc4179dfa5be36c1772c07154850cf213
남현우 <hwnim5324@naver.com>
2022-07-02 23:42:00 +0900

    users rest api in progress.

---
+++ src/main/java/berich/service/UserService.java
@@ -0,0 +1,14 @@
+package berich.service;
+
+import berich.DTO.UserDTO;
+
+public class UserService {
+    private UserDTO user;
+
+    public int login(){
+        int stateCode = 0;
+
+        return -1;
+    }
+
+}
