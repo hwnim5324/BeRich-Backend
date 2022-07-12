b08b70f7fa59564e936d46b27ffcc3820a62e9cc
남현우 <hwnim5324@naver.com>
2022-07-12 14:44:38 +0900

    set dto for stocks

---
+++ src/main/java/berich/service/StocksDTO.java
@@ -0,0 +1,49 @@
+package berich.service;
+
+public class StocksDTO {
+    private int Stock;
+    private int HGPR;   //고가
+    private int LWPR;   //저가
+    private int OPRC;   //시가
+    private int CLPR;   //종가
+
+    public int getStock() {
+        return Stock;
+    }
+
+    public void setStock(int stock) {
+        Stock = stock;
+    }
+
+    public int getHGPR() {
+        return HGPR;
+    }
+
+    public void setHGPR(int HGPR) {
+        this.HGPR = HGPR;
+    }
+
+    public int getLWPR() {
+        return LWPR;
+    }
+
+    public void setLWPR(int LWPR) {
+        this.LWPR = LWPR;
+    }
+
+    public int getOPRC() {
+        return OPRC;
+    }
+
+    public void setOPRC(int OPRC) {
+        this.OPRC = OPRC;
+    }
+
+    public int getCLPR() {
+        return CLPR;
+    }
+
+    public void setCLPR(int CLPR) {
+        this.CLPR = CLPR;
+    }
+}
