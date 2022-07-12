64e87c854448190ac2f21545126ae675f997bab3
남현우 <hwnim5324@naver.com>
2022-07-12 14:45:12 +0900

    change jpa settings for orderlist

--- src/main/java/berich/service/OrderlistService.java
+++ src/main/java/berich/service/OrderlistService.java
@@ -1,8 +1,12 @@
 package berich.service;
 
+import berich.DTO.OrderlistDTO;
+import berich.DTO.UserDTO;
 import berich.repository.JPAOrderlistRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class OrderlistService {
@@ -11,8 +15,17 @@ public class OrderlistService {
     @Autowired
     public OrderlistService(JPAOrderlistRepository jpaorderlistrepository) {
         this.jpaorderlistrepository = jpaorderlistrepository;
+    }
 
+    public OrderlistDTO create(OrderlistDTO orderlist){
+        return jpaorderlistrepository.save(orderlist);
+    }
 
+    public Optional<OrderlistDTO> read(int usercode){
+        return jpaorderlistrepository.findById(usercode);
+    }
 
+    public OrderlistDTO update(OrderlistDTO orderlist){
+        return jpaorderlistrepository.save(orderlist);
     }
 }
