0e948e9b1eb1a273e043fdc596654d8a98c3925f
남현우 <hwnim5324@naver.com>
2022-07-19 23:54:32 +0900

    Stock service in progress.

--- src/main/java/berich/service/StockService.java
+++ src/main/java/berich/service/StockService.java
@@ -1,6 +1,12 @@
 package berich.service;
 
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
 import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
@@ -8,6 +14,41 @@ import java.util.Arrays;
 import java.util.List;
 
 public class StockService {
+    private static Secret SECRET = new Secret();
+    private String TOKEN = "";
+
+    public StockService() {
+        this.TOKEN = getTokenByKIS();
+    }
+
+    private String getTokenByKIS(){
+        String KIS = "https://openapi.koreainvestment.com:9443/oauth2/tokenP";
+
+        try{
+            URL url = new URL(KIS);
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+
+            con.setRequestMethod("POST");
+            con.setRequestProperty("grant_type", "client_credentials");
+            con.setRequestProperty("appkey", SECRET.getAPPKEY());
+            con.setRequestProperty("appsecret", SECRET.getAPPSECRET());
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            String response = br.readLine();
+
+            JSONParser parser = new JSONParser();
+            JSONObject obj = new JSONObject();
+
+            obj = (JSONObject) parser.parse(response.toString());
+            System.out.println("Get Token Success.");
+            return obj.get("access_token").toString();
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        System.out.println("Faild to get Token");
+        return "Can't Find";
+    }
 
     public String readByName(String isnm){
         String CSV_PATH = "../../../resources/assets/StockList.csv";
@@ -32,13 +73,12 @@ public class StockService {
         return "Can't Find";
     }
 
-    public String transferWithKIS(int iscd){
-        String URL = "https://openapi.koreainvestment.com:9443/uapi/domestic-stock/v1/quotations/inquire-daily-itemchartprice";
-
-
+    public String getStockPrice(int iscd){
 
         return "";
     }
+
+
 
 
 }
